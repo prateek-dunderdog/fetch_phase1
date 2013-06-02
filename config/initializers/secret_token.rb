4f9f6fb2330094bd91a6fd5eddf3815a93c39068
msuser1 <msuser1@mslap5.(none)>
2013-06-02 22:00:47 +0530

    Spree integration with application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FetchPhase1::Application.config.secret_token = '9bb54f7b28a82db7bae0c3221294a3a0cc355946c0125bddfbc93242b4f99d44b155ac1418b56433a57de216ce3216643c8dec1eb4778a68f1b4c3f28bd49edd'
